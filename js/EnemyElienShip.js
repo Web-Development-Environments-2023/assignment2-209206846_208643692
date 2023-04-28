e248ddcc6ef7bb8e820381dbda7d3e4ae9839b0f
Gad Miller <gadmiller7@gmail.com>
2023-04-28 16:37:31 +0300

    change location

---
+++ js/EnemyElienShip.js
@@ -0,0 +1,54 @@
+class EnemyElienShip{
+
+	constructor(points){
+        this.image = new Image();
+		if (points == 5){
+			this.image.src = "./images/ElienShip.png"
+			this.image.width = window.innerWidth * 0.056
+			this.image.height = window.innerHeight * 0.112
+			this.width = window.innerWidth * 0.056
+			this.height = window.innerHeight * 0.112
+		}
+
+		if (points == 10){
+			this.image.src = "./images/Alien1.png"
+			this.image.width = window.innerWidth * 0.056
+			this.image.height = window.innerHeight * 0.112
+			this.width = window.innerWidth * 0.056
+			this.height = window.innerHeight * 0.112
+		}
+
+		if (points == 15){
+			this.image.src = "./images/Alien2.png"
+			this.image.width = window.innerWidth * 0.056
+			this.image.height = window.innerHeight * 0.112
+			this.width = window.innerWidth * 0.056
+			this.height = window.innerHeight * 0.112
+		}
+
+		if (points == 20){
+			this.image.src = "./images/Alien3.png"
+			this.image.width = window.innerWidth * 0.056
+			this.image.height = window.innerHeight * 0.112
+			this.width = window.innerWidth * 0.056
+			this.height = window.innerHeight * 0.112
+		}
+
+		this.points = points
+
+		this.position = {
+			x: 0,
+			y: 0
+		}
+
+		this.sides = {
+			bottom: this.position.y + this.height
+		}
+
+	}
+
+	draw(){
+        c.drawImage(this.image,this.position.x,this.position.y,this.image.width,this.image.height)
+	}
+}
+
---
+++ js/EnemyShipsConroller.js
@@ -0,0 +1,103 @@
+class EnemyShipsConroller{
+
+    constructor(numberOfEnemiesShips, bulletController, enemiesBulletController){
+        // this.canvas = canvas
+        this.enemyElienShips = [];
+        this.moveEnemiesRight_or_Left = true;
+        this.bulletControllerOfMainPlayer = bulletController
+        this.enemiesBulletController = enemiesBulletController
+
+        for(let i=0; i<numberOfEnemies ; i++){
+            let score = 0
+            if (i < 5){
+                score = 20
+            }
+            else if (i < 10){
+                score = 15
+            }
+            else if (i < 15){
+                score = 10
+            }
+            else if (i < 20){
+                score = 5
+            }
+            this.enemyElienShips.push(new EnemyElienShip(score))}
+        this.numberOfEnemiesShips = numberOfEnemiesShips
+        this.positionTheShips()
+        this.currentBulletSpeed = 2.5
+        this.currentXChange = window.innerWidth * 0.0004
+        this.playerScore = 0
+    }
+
+    draw(){
+        this.makeEnemiesMoveLeftRightAndCheckHits()
+    }
+
+    
+    positionTheShips(){
+        let col_x_position = 0
+        let row_y_position = 0
+       for(let i=0; i< this.enemyElienShips.length ; i++){    
+            if (i !== 0 && i%5 == 0){
+                // each row there is 5 ships update x,y of the ships
+                row_y_position++ 
+                col_x_position = 0 
+            }
+            this.enemyElienShips[i].position.x = col_x_position*window.innerWidth * 0.08
+            this.enemyElienShips[i].position.y = row_y_position*window.innerHeight * 0.096
+            col_x_position++ // so each one of the five ships will be on thse place col x
+            
+       }
+    }
+
+
+    makeEnemiesMoveLeftRightAndCheckHits(){
+        // this.draw()
+        for(let i=0; i<this.enemyElienShips.length ; i++){
+            // console.log("hit1")
+            if (this.bulletControllerOfMainPlayer.collideWith(this.enemyElienShips[i])){
+                laserSound.play()
+                this.playerScore += this.enemyElienShips[i].points
+                this.enemyElienShips.splice(i,1)
+                console.log("hit")
+                // add sound
+            }
+            else{
+    
+            if(this.enemyElienShips[i].position.x <= window.innerWidth * 0.008){
+                this.moveEnemiesRight_or_Left = true
+            }
+            else if(this.enemyElienShips[i].position.x >= canvas.width-window.innerWidth * 0.08){
+                this.moveEnemiesRight_or_Left = false
+            }
+    
+            if (this.moveEnemiesRight_or_Left){
+                this.enemyElienShips[i].position.x += this.currentXChange
+            }
+            else if (!this.moveEnemiesRight_or_Left){
+                this.enemyElienShips[i].position.x -= this.currentXChange
+            }
+            
+            this.enemyElienShips[i].draw();
+        }
+    
+        }
+    }
+
+    shoot(){
+        // choose a random ship
+        let ships_num = this.enemyElienShips.length;
+        let shootingShip = this.enemyElienShips[Math.floor(Math.random() * ships_num)];
+
+        const speed = this.currentBulletSpeed;
+        const damage = 1;
+        const bulletX = shootingShip.position.x;
+        const bulletY = shootingShip.position.y;
+        const w = window.innerHeight * 0.008;
+        const h = window.innerHeight * 0.016;
+        const color = "green"
+
+        this.enemiesBulletController.shoot(bulletX, bulletY, speed, damage, w, h, color);
+    }
+}
+
